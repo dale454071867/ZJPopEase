fafc543510a81f5be8f78c1fbdaba87212af17f2
zhoujie <454071867@qq.com>
2015-07-09 13:42:40 +0800

    createNew

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJPopEase
+//
+//  Created by 周杰 on 15/7/9.
+//  Copyright (c) 2015年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
